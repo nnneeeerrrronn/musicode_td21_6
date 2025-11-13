2e38b51ccabadfb29911a712d314290d10aa532c
Aurélia Boury <i2401223@cri.univ-lemans.fr>
2025-11-13 12:25:58 +0100

    pour se co a la bdd avec formulaire

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+
+
+$host = 'localhost';
+
+$dbname = 'musicode_td21_6';
+
+$username = 'root';
+
+$password = '';
+
+
+
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    die("Erreur de connexion : " . $e->getMessage());
+
+}
+
+?>
